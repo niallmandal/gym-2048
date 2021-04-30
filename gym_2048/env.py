adc679fd8c6770a638c0729b1a9330910d01c58b
niallmandal <niall@mandal.us>
2021-04-30 10:48:37 -0700

    Update env.py

--- gym_2048/env.py
+++ gym_2048/env.py
@@ -57,6 +57,12 @@ class Base2048Env(gym.Env):
     
     if np.array_equal(updated_obs, self.board):
       print("No board change")
+      for i in range(50):
+        action = random.choice([0,1,2,3])
+        rotated_obs = np.rot90(self.board, k=action)
+        reward, updated_obs = self._slide_left_and_merge(rotated_obs)
+        if np.array_equal(updated_obs, self.board) == False:
+          break
      
     self.board = np.rot90(updated_obs, k=4 - action)
     
